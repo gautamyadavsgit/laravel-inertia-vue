876d42ccee42b41dfb62e11f68bc9771682633f8
gautamyadavsgit <gautaminit@gmail.com>
2023-04-08 21:44:29 +0530

    added starter

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <Link href="/">Main Page</Link>&nbsp;
+  <Link href="/show">Show Page</Link>
+  <div>The page with time {{ timer }}</div>
+
+  <slot />
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
+const timer = ref(1)
+
+setInterval(() => {
+  timer.value++
+}, 1000)
+</script>
+
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Index The message is : {{ messageFrom }}</div>
+  <Link href="/show" />
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+defineProps({ messageFrom: String })
+</script>
